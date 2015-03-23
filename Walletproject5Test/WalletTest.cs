107263eda66243992ebe6c1fa20f2ff16a32ba10
hhodzi00 <hhodzi00@fesb.hr>
2015-03-23 10:51:07 +0100

    kakaka

---
+++ Walletproject5Test/WalletTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using  walletproject5;
+
+namespace Walletproject5Test
+{
+    [TestClass]
+    public class WalletTest
+    {
+        [TestMethod]
+        public void TestEmptyWalletReturnsZero()
+        {
+            var wallet = new Wallet();
+
+            Assert.AreEqual
+            (
+                0,
+                wallet.GetCurrencyValue("HRK"),
+                "Empty wallet should return 0"
+            );
+        }
+        [TestMethod]
+        public void TestSingleItemWalletGetCurrency()
+        {
+            var wallet = new Wallet();
+
+            wallet.AddCurrencyValue("HRK", 100);
+
+
+            Assert.AreEqual
+               (
+                   100,
+                   wallet.GetCurrencyValue("HRK"),
+                   "Single item wallet sahould return previously added"
+               );
+
+
+        }
+
+
+
+        [TestMethod]
+        public void TestMultipleSameCurrencyWalletGetCurrency()
+        {
+            var wallet = new Wallet();
+
+            wallet.AddCurrencyValue("HRK", 100);
+            wallet.AddCurrencyValue("HRK", 50);
+
+            Assert.AreEqual
+               (
+                   150,
+                   wallet.GetCurrencyValue("HRK"),
+                   "should aggregate added values"
+               );
+
+
+        }
+
+
+
+        [TestMethod]
+        [ExpectedException(typeof (InvalidProgramException))]
+        public void TestAddCurrencyCrashesIfFormatLengthInvalid()
+        {
+            var wallet = new Wallet();
+
+            wallet.AddCurrencyValue("HR", 100);
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ walletproject5/Wallet.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace walletproject5
+{
+   public class Wallet
+    {
+       public Wallet()
+       {
+           this.dictionary= new Dictionary<string,float>();
+       }
+
+       public void AddCurrencyValue(string currency, float value)
+       {
+           CheckCurrencyFormat(currency);
+
+
+           if(this.dictionary.ContainsKey(currency))
+           {
+            this.dictionary[currency] += value;
+           }
+           else 
+
+           this.dictionary.Add(currency, value);
+       }
+
+       private void CheckCurrencyFormat(string currency)
+       {
+           if (currency == null) { throw new InvalidProgramException("currency can not be null"); }
+
+           if (currency.Length != 3)
+           {
+            throw new InvalidProgramException("currency mudt be of length 3");
+           }
+
+           if (currency.ToUpper() != currency)
+           {
+               throw new InvalidProgramException("currency must be upper case");
+           }
+
+       }
+
+       public float GetCurrencyValue( string currency)
+       {
+           return this.dictionary.ContainsKey(currency)
+               ? this.dictionary[currency]
+               : 0;
+       }
+
+
+       public string GetState()
+       {
+           var state = "";
+
+           for (var i = 0; i < this.dictionary.Keys.Count; i++)
+           {
+               var key = this.dictionary.Keys.ElementAt(i); //HRK
+               var value = this.dictionary[key];        //100
+           }
+
+           return state;
+       }
+
+
+       private Dictionary<string, float> dictionary;
+    }
+}
